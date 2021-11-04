8ca4c9270ba0bbbea1ff431907e37268a32c0d9a
demba <demba.pro.76000@gmail.com>
2021-11-04 14:06:55 +0100

    competences_augmentation

--- src/components/knowledges/Languages.js
+++ src/components/knowledges/Languages.js
@@ -5,17 +5,22 @@ import ProgressBar from './ProgressBar';
 class Languages extends Component {
     state = {
         languages: [
-            { id: 1, value: "Javascript", xp: 0.4 },
-            { id: 2, value: "Css", xp: 0.5 },
-            { id: 3, value: "Php", xp: 0.3 },
-            { id: 4, value: "My sql", xp: 0.4 }
+            { id: 1, value: "Javascript", xp: 1 },
+            { id: 2, value: "Css", xp: 1},
+            { id: 3, value: "Php", xp: 1},
+            { id: 4, value: "My sql", xp: 1 },
+            { id: 5, value: "html", xp: 1 }
+
+
 
         ],
         frameworks: [
             { id: 1, value: "React", xp: 0.4 },
             { id: 2, value: "Boostrap", xp: 0.3 },
             { id: 3, value: "Sass", xp: 0.5 },
-            { id: 4, value: "React", xp: 0.5 }
+            { id: 4, value: "TypeScript", xp: 0.5 },
+            { id: 5, value: "NextJS", xp: 0.5 }
+
         ],
     }
 
