ddf1bea66068f64ed763d639b59be82a8d1f1290
demba <74179850+DembaMBOW@users.noreply.github.com>
2022-01-06 21:06:34 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Knowledges from './pages/Knowledges';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import NoteFound from './pages/NoteFound';
+import { Switch, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { lazy, Suspense } from "react";
+import GlobalStyle from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import Loading from "./subComponents/Loading";
 
-const App = () => {
+//Components
+const Main = lazy(() => import("./components/Main"));
+const AboutPage = lazy(() => import("./components/AboutPage"));
+const MySkillsPage = lazy(() => import("./components/MySkillsPage"));
+const BlogPage = lazy(() => import("./components/BlogPage"));
+const WorkPage = lazy(() => import("./components/WorkPage"));
+const SoundBar = lazy(() => import("./subComponents/SoundBar"));
+
+function App() {
+  const location = useLocation();
+
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/competences" component={Knowledges} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NoteFound} />
-        </Switch>
-      </BrowserRouter>
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <Suspense fallback={<Loading />}>
+          <SoundBar />
+
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/" component={Main} />
+
+              <Route exact path="/about" component={AboutPage} />
+
+              <Route exact path="/blog" component={BlogPage} />
+
+              <Route exact path="/work" component={WorkPage} />
+
+              <Route exact path="/skills" component={MySkillsPage} />
+
+
+
+            </Switch>
+          </AnimatePresence>
+        </Suspense>
+      </ThemeProvider>
+    </>
   );
-};
+}
 
 export default App;
