3e48c75a0578ffdd8f571c5c3f150c9d9d78ea90
demba <74179850+DembaMBOW@users.noreply.github.com>
2022-01-09 16:33:46 +0100

    problème-affichage

--- src/components/BlogData.js
+++ src/components/BlogData.js
@@ -1,89 +1,89 @@
-export const Blogs = [
-    {
-        id:1,
-        name:"Mussée d'oeuvres Réaliser avec Photoshop ",
-        tags:[],
-        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        // link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10",
-        img: "/assets/Images/musée-palais-MEAM-portofolio.jpg",
-        info: "zertfghrjtuyh",
-    },
-    {
-        id:2,
-        name:"Musée d'oeuvres de certains project non actifs avec aussi mes passion Réaliser avec Phtoshop",
-        tags:[],
-        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        // link:"https://codebucks.hashnode.dev/react-hamburger-menu",
-        // picture: "/.assets/images/muséedemba.png"
-        img: "/assets/Images/muséedemba.png",
-
-        
-    },
-    {
-        id:3,
-        name:"Gif Animer Réaliser avec Phtoshop",
-        tags:["Montant une échelle"],
-        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
-        img: "/assets/Images/gif-animé.gif",
-
-    },
-    {
-        id:4,
-        name:"Motivation",
-        tags:[],
-        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
-        // link:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
-
-        img: "/assets/Images/53-Pere-riche-Pere-pauvre-Robert-Kiyosaki.jpg",
-
-    },
-    {
-        id:5,
-        name:"J'aime les mangas",
-        tags:[],
-        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
-        img: "/assets/Images/th.png",
-
-    },
-    {
-        id:6,
-        name:"Je suis passioné par le Sport",
-        tags:[],
-        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        // link:"https://codebucks.hashnode.dev/pagination-in-react"
-        img: "/assets/Images/depositphotos_13625743-stock-photo-assorted-sports-equipment-on-black.jpg",
+// export const Blogs = [
+//     {
+//         id:1,
+//         name:"Mussée d'oeuvres Réaliser avec Photoshop ",
+//         tags:[],
+//         // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+//         // link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10",
+//         img: "/assets/Images/musée-palais-MEAM-portofolio.jpg",
+//         info: "zertfghrjtuyh",
+//     },
+//     {
+//         id:2,
+//         name:"Musée d'oeuvres de certains project non actifs avec aussi mes passion Réaliser avec Phtoshop",
+//         tags:[],
+//         // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+//         // link:"https://codebucks.hashnode.dev/react-hamburger-menu",
+//         // picture: "/.assets/images/muséedemba.png"
+//         img: "/assets/Images/muséedemba.png",
 
 
-    }
-   
-  
-    // {
-    //     id:8,
-    //     name:"Basket-ball",
-    //     tags:[],
-    //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
+//     },
+//     {
+//         id:3,
+//         name:"Gif Animer Réaliser avec Phtoshop",
+//         tags:["Montant une échelle"],
+//         // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+//         // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
+//         img: "/assets/Images/gif-animé.gif",
 
-    // },
-    // {
-    //     id:9,
-    //     name:"",
-    //     tags:[],
-    //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
+//     },
+//     {
+//         id:4,
+//         name:"Motivation",
+//         tags:[],
+//         // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
+//         // link:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
 
-    // },
-    // {
-    //     id:10,
-    //     name:"Père Riche Père",
-    //     tags:[],
-    //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
-    //     img: "/assets/Images/53-Pere-riche-Pere-pauvre-Robert-Kiyosaki.jpg",
+//         img: "/assets/Images/53-Pere-riche-Pere-pauvre-Robert-Kiyosaki.jpg",
 
-    // },
-  
-  
-] 
+//     },
+//     {
+//         id:5,
+//         name:"J'aime les mangas",
+//         tags:[],
+//         // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+//         // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
+//         img: "/assets/Images/th.png",
+
+//     },
+//     {
+//         id:6,
+//         name:"Je suis passioné par le Sport",
+//         tags:[],
+//         // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+//         // link:"https://codebucks.hashnode.dev/pagination-in-react"
+//         img: "/assets/Images/depositphotos_13625743-stock-photo-assorted-sports-equipment-on-black.jpg",
+
+
+//     }
+
+
+//     // {
+//     //     id:8,
+//     //     name:"Basket-ball",
+//     //     tags:[],
+//     //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+//     //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
+
+//     // },
+//     // {
+//     //     id:9,
+//     //     name:"",
+//     //     tags:[],
+//     //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+//     //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
+
+//     // },
+//     // {
+//     //     id:10,
+//     //     name:"Père Riche Père",
+//     //     tags:[],
+//     //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+//     //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
+//     //     img: "/assets/Images/53-Pere-riche-Pere-pauvre-Robert-Kiyosaki.jpg",
+
+//     // },
+
+
+// ]
