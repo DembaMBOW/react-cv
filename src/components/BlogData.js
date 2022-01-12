8db5436e9a78cc56ae698abece307d5f4247529f
demba <74179850+DembaMBOW@users.noreply.github.com>
2022-01-12 15:39:38 +0100

    test-changement-gif

--- src/components/BlogData.js
+++ src/components/BlogData.js
@@ -5,7 +5,6 @@ export const Blogs = [{
         // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
         link: "https://dembamb.com/",
         img: "/assets/Images/musée-palais-MEAM-portofolio.jpg",
-        info: "zertfghrjtuyh",
     },
     {
         id: 2,
@@ -18,15 +17,15 @@ export const Blogs = [{
 
 
     },
-    //     {
-    //         id:3,
-    //         name:"Gif Animer Réaliser avec Phtoshop",
-    //         tags:["Montant une échelle"],
-    //         // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //         // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
-    //         img: "/assets/Images/gif-animé.gif",
+    {
+        id: 3,
+        name: "Gif Animer Réaliser avec Phtoshop",
+        tags: ["Montant une échelle"],
+        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
+        img: "/assets/Images/gif-bleu11.gif",
 
-    //     },
+    },
 
 
 
