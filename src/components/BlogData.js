67d906a0219d086712dd61cae368aff2a04fc88c
demba <74179850+DembaMBOW@users.noreply.github.com>
2022-01-12 02:50:14 +0100

    ajout-page-blog

--- src/components/BlogData.js
+++ src/components/BlogData.js
@@ -34,9 +34,21 @@ export const Blogs = [{
         // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
         // link:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
 
-        img: "/assets/Images/53-Pere-riche-Pere-pauvre-Robert-Kiyosaki.jpg",
+        img: "/assets/Images/Père-riche-pere-pauvre-copie-1024x585.png",
 
     },
+
+    {
+        id: 5,
+        name: "Logo requin Réaliser avec Photoshop avec Adobe Illustrator",
+        tags: [],
+        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+        link: "https://dembamb.com/",
+        img: "/assets/Images/LOGO-SHark.png"
+
+
+    },
+
     // {
     //     id: 5,
     //     name: "J'aime les mangas",
@@ -46,16 +58,16 @@ export const Blogs = [{
     //     img: "/assets/Images/th.png",
 
     // },
-    {
-        id: 6,
-        name: "Je suis passioné par le Sport",
-        tags: [],
-        // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        // link:"https://codebucks.hashnode.dev/pagination-in-react"
-        img: "/assets/Images/depositphotos_13625743-stock-photo-assorted-sports-equipment-on-black.jpg",
+    // {
+    //     id: 6,
+    //     name: "Je suis passioné par le Sport",
+    //     tags: [],
+    //     // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    //     // link:"https://codebucks.hashnode.dev/pagination-in-react"
+    //     img: "/assets/Images/depositphotos_13625743-stock-photo-assorted-sports-equipment-on-black.jpg",
 
 
-    }
+    // },
 
 
     //     // {
@@ -74,15 +86,6 @@ export const Blogs = [{
     //     //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
 
     //     // },
-    // {
-    //     id: 10,
-    //     name: "Père Riche Père",
-    //     tags: [],
-    //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
-    //     img: "/assets/Images/53-Pere-riche-Pere-pauvre-Robert-Kiyosaki.jpg",
-
-    // },
 
 
 ]
