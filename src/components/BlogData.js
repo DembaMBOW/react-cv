4bde98ca10411461aea62fd06bfbacea2e47e6c1
demba <74179850+DembaMBOW@users.noreply.github.com>
2022-01-12 14:56:42 +0100

    deplacement-slide

--- src/components/BlogData.js
+++ src/components/BlogData.js
@@ -27,19 +27,11 @@ export const Blogs = [{
     //         img: "/assets/Images/gif-animé.gif",
 
     //     },
+
+
+
     {
         id: 4,
-        name: "Motivation",
-        tags: [],
-        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
-        // link:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
-
-        img: "/assets/Images/Père-riche-pere-pauvre-copie-1024x585.png",
-
-    },
-
-    {
-        id: 5,
         name: "Logo requin Réaliser avec Photoshop et Illustrator",
         tags: [],
         // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
@@ -49,43 +41,16 @@ export const Blogs = [{
 
     },
 
-    // {
-    //     id: 5,
-    //     name: "J'aime les mangas",
-    //     tags: [],
-    //     // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //     // link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
-    //     img: "/assets/Images/th.png",
+    {
+        id: 5,
+        name: "Ma source de Motivation",
+        tags: [],
+        // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
+        // link:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.pnghttps://www.youtube.com/watch?v=Ze5jSeX4Iwshttps://dev.to/codebucks/what-is-redux-simply-explained-2ch7",
 
-    // },
-    // {
-    //     id: 6,
-    //     name: "Je suis passioné par le Sport",
-    //     tags: [],
-    //     // imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //     // link:"https://codebucks.hashnode.dev/pagination-in-react"
-    //     img: "/assets/Images/depositphotos_13625743-stock-photo-assorted-sports-equipment-on-black.jpg",
+        img: "/assets/Images/Père-riche-pere-pauvre-copie-1024x585.png",
 
-
-    // },
-
-
-    //     // {
-    //     //     id:8,
-    //     //     name:"Basket-ball",
-    //     //     tags:[],
-    //     //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
-
-    //     // },
-    //     // {
-    //     //     id:9,
-    //     //     name:"",
-    //     //     tags:[],
-    //     //     // imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     //     // link:"C:\Users\Demba\Desktop\Tout\img\sans-lokcs.png"
-
-    //     // },
+    },
 
 
 ]
